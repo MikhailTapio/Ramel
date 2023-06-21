050ecf99d592c433b2351e3d05d45de6cbe58036
MikhailTapio <mikhailtapio13468@163.com>
2023-06-21 16:52:15 +0800

    Ram ram!

---
+++ src/main/java/committee/nova/ramel/Ramel.java
@@ -0,0 +1,35 @@
+package committee.nova.ramel;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.network.NetworkConstants;
+
+@Mod("ramel")
+public class Ramel {
+    private static final ForgeConfigSpec CFG;
+    public static final ForgeConfigSpec.DoubleValue ramImpactRange;
+    public static final ForgeConfigSpec.DoubleValue ramDamage;
+    public static final ForgeConfigSpec.DoubleValue ramKnockBackMultiplier;
+
+    static {
+        final var builder = new ForgeConfigSpec.Builder();
+        builder.comment("Ramel Settings").push("general");
+        ramImpactRange = builder.comment("Additional radius of area affected by a camel's ramming. For adult camels, the radius is twice")
+                .defineInRange("ramImpactRange", 0.5, 0.0, 2.5);
+        ramDamage = builder.comment("The base damage of a camel's ramming, an adult camel's ramming can deal twice as much damage.")
+                .defineInRange("ramDamage", 1.0, 0.0, 20.0);
+        ramKnockBackMultiplier = builder.comment("The multiplier of the force of knockback dealt by a camel's ramming")
+                .defineInRange("ramKnockBackMultiplier", 1.0, 0.0, 5.0);
+        builder.pop();
+        CFG = builder.build();
+    }
+
+    public Ramel() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CFG);
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+                () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (r, s) -> true));
+    }
+}
---
+++ src/main/java/committee/nova/ramel/mixin/MixinCamel.java
@@ -0,0 +1,46 @@
+package committee.nova.ramel.mixin;
+
+import committee.nova.ramel.Ramel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.Mth;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.camel.Camel;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Objects;
+
+@Mixin(Camel.class)
+public abstract class MixinCamel extends LivingEntity {
+    @Shadow
+    public abstract boolean isDashing();
+
+    protected MixinCamel(EntityType<? extends LivingEntity> t, Level w) {
+        super(t, w);
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void inject$tick(CallbackInfo ci) {
+        if (!isDashing()) return;
+        final int i = hasEffect(MobEffects.MOVEMENT_SPEED) ? Objects.requireNonNull(getEffect(MobEffects.MOVEMENT_SPEED)).getAmplifier() + 1 : 0;
+        final int j = hasEffect(MobEffects.MOVEMENT_SLOWDOWN) ? Objects.requireNonNull(getEffect(MobEffects.MOVEMENT_SLOWDOWN)).getAmplifier() + 1 : 0;
+        final double impactBySpeed = Mth.clamp(getSpeed() * 1.65, .2, 3.0) + .25 * (i - j);
+        level().getEntities(this, getBoundingBox().inflate(Ramel.ramImpactRange.get() * (isBaby() ? 1.0 : 2.0)), Entity::isAlive).stream()
+                .filter(e -> e instanceof LivingEntity && !getPassengers().contains(e))
+                .forEach(e -> {
+                    final LivingEntity l = (LivingEntity) e;
+                    e.playSound(SoundEvents.PLAYER_ATTACK_KNOCKBACK);
+                    e.hurt(damageSources().noAggroMobAttack(this), Ramel.ramDamage.get().floatValue() * (isBaby() ? 1.0F : 2.0F));
+                    final double blockedImpact = l.isDamageSourceBlocked(damageSources().mobAttack(this)) ? .5 : 1.0;
+                    l.knockback(blockedImpact * impactBySpeed * Ramel.ramKnockBackMultiplier.get() * (isBaby() ? 1.0 : 2.5),
+                            Mth.sin(getYRot() * ((float) Math.PI / 180)), -Mth.cos(getYRot() * ((float) Math.PI / 180)));
+                });
+    }
+}
